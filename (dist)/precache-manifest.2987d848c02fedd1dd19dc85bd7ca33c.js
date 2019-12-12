e99db067306e7f495e0fb868da54c446f7023cbc
Cassandra Dacha <44982109+CassandraDacha@users.noreply.github.com>
2019-12-12 04:50:32 +0200

    Add files via upload

---
+++ (dist)/precache-manifest.2987d848c02fedd1dd19dc85bd7ca33c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d68f0f1596ddef9d310cc33d0c404cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da8941526d824b3dcdb2",
+    "url": "/static/css/main.edbb531f.chunk.css"
+  },
+  {
+    "revision": "590cf06240b094f0afef",
+    "url": "/static/js/2.c211763c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c211763c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da8941526d824b3dcdb2",
+    "url": "/static/js/main.61a7ae53.chunk.js"
+  },
+  {
+    "revision": "8932fdb2331e7d4035e6",
+    "url": "/static/js/runtime-main.6c2ae1b3.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/static/media/pokeball.f10bf078.png"
+  }
+]);
